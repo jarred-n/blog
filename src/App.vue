4c8a268e00802dcb5d8b492306cdaeacb6cc3083
jarred <hhuanjia86@gmail.com>
2019-10-13 11:39:28 +0800

    配置服务端，导出数据库表结构

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
+<!-- APP.vue -->
+
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <router-view></router-view> <!-- 原本的Login换成了router-view 这就是路由视图渲染的目标元素-->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'app' // 不需要再引入`Login`\`TodoList`组件了，因为在路由里已经注册了
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,37 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// src/main.js
+
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(VueRouter);
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+import Login from './components/Login'
+import TodoList from './components/TodoList'
+
+const router =  new VueRouter({
+  mode: 'history', // 开启HTML5的history模式，可以让地址栏的url长得跟正常页面跳转的url一样。（不过还需要后端配合，讲Koa的时候会说）
+  base: __dirname,
+  routes: [
+    {
+      path: '/',  // 默认首页打开是登录页
+      component: Login
+    },
+    {
+      path: '/todolist',
+      component: TodoList
+    },
+    {
+      path: '*',
+      redirect: '/' // 输入其他不存在的地址自动跳回首页
+    }
+  ]
 })
+
+const app = new Vue({
+  router: router, // 启用router
+  render: h => h(App)
+}).$mount('#app') //挂载到id为app的元素上
